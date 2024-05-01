24200c898b1e6994c5d52e7a2d9df410e2c7767a
Hi10w <145377647+Hi10w@users.noreply.github.com>
2024-05-01 09:56:51 -0700

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,36 +1,31 @@
-
-
 let counter = 0;
 let submitButton = document.querySelector(".submit");
 let messages2 = document.querySelector(".messages2");
-let messages = document.querySelector(".messages");
+let messages1 = document.querySelector(".messages1");
 let quizResult = document.querySelector(".results");
-   
+let heightInput =
+    document.querySelector(".height").value;
+let cleatInput =
+    document.querySelector(".cleat").value;
+console.log(heightInput);
 
-    
+console.log(counter);
 submitButton.onclick = function() {
     counter = counter + 1;
-    console.log(counter);
-    let heightInput =
-        document.querySelector(".height").value;
-    let cleatInput =
-        document.querySelector(".cleat").value;
-    console.log(heightInput);
-    messages.innerHTML = "userComment";
-    
-    
-        messages2.innerHTML = " you did the quiz this many times " + counter;
-    
-    if (cleatInput === "yes" && heightInput >= 67) {
-        messages.innerHTML = "American football";
-    } else if (cleatInput === "no" && heightInput >= 67) {
-        messages.innerHTML = "Basketball";
-    } else if (cleatInput === "no" && heightInput <= 67) {
-        messages.innerHTML = "you dont play sports";
-    } else if (cleatInput === "yes" && heightInput <= 67) {
-        messages.innerHTML = "soccer";
+
+
+
+
+    if (cleatInput === "yes" && heightInput >= 6, 0) {
+        messages1.innerHTML = "American football";
+    } else if (cleatInput === "no" && heightInput >= 6, 0) {
+        messages1.innerHTML = "Basketball";
+    } else if (cleatInput === "no" && heightInput <= 6, 0) {
+        messages1.innerHTML = "you dont play sports";
+    } else if (cleatInput === "yes" && heightInput <= 6, 0) {
+        messages1.innerHTML = "soccer";
     }
- 
+    messages2.innerHTML = " you did the quiz this many times " + counter;
 
 
 };
